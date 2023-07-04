5746e2be7040a15a157ad66529f6e027e9b5dc21
Moonsun23 <socoooool90@gmail.com>
2023-07-04 11:06:41 +0900

    프로젝트 초기화

---
+++ src/test/java/hi1237/hello/boot/spring5boot/Spring5BootApplicationTests.java
@@ -0,0 +1,13 @@
+package hi1237.hello.boot.spring5boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
