4fe5c2e93bac14d1eca30f76fb561a091cc91b1d
Moonsun23 <socoooool90@gmail.com>
2023-07-06 15:51:03 +0900

    회원가입 : 실명 확인 처리용 PostMapping 추가

--- src/main/java/hi1237/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/JoinController.java
@@ -1,10 +1,14 @@
 package hi1237.hello.boot.spring5boot.controller;
 
+import hi1237.hello.boot.spring5boot.model.Checkme;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/join")
@@ -21,9 +25,26 @@ public class JoinController {
     }
     @GetMapping("/checkme")
     public String checkme(){
+
         logger.info("checkme 호출!");
 
         return "join/checkme";
+
+    }
+    @PostMapping("/checkme")
+    public String checkmeok(Checkme checkme, HttpSession sess){
+        // Checkme 안에 string 추가를 위해 model에 checkme라는 클래스를 만들어줌
+        logger.info("join/checkmeok 호출!");
+        // checkme에 보낸 개인정보를 세션에 저장하고
+        // joinme로 이동
+        String viewPage="redirect:/join/checkme";
+
+        if(checkme.getName() !=null){
+            sess.setAttribute("checkme", checkme);
+            viewPage="redirect:/join/joinme";
+        }
+
+        return viewPage;
 
     }
     @GetMapping("/joinme")
