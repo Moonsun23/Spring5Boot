dc45d9c2a6bede3d246dbc8ec8117d33adaebe2e
Moonsun23 <socoooool90@gmail.com>
2023-07-07 10:38:36 +0900

    회원가입 : Member 클래스 추가

---
+++ src/main/java/hi1237/hello/boot/spring5boot/dao/Member.java
@@ -0,0 +1,23 @@
+package hi1237.hello.boot.spring5boot.dao;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Member {
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String phone;
+    private String regdate;
+}
