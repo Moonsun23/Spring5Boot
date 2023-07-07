2770038a8142970b77525f6ed731b148b827531f
Moonsun23 <socoooool90@gmail.com>
2023-07-07 16:22:54 +0900

    우편번호 조회 : mybatis findZipcode 맵핑 추가

--- src/main/java/hi1237/hello/boot/spring5boot/mybatis/MemberMapper.java
+++ src/main/java/hi1237/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -1,6 +1,7 @@
 package hi1237.hello.boot.spring5boot.mybatis;
 
 import hi1237.hello.boot.spring5boot.model.Member;
+import hi1237.hello.boot.spring5boot.model.Zipcode;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface MemberMapper {
 
     int insertMember(Member m);
     List<Member> selectMember();
+
+    List<Zipcode> findZipcode(String dong);
 }
