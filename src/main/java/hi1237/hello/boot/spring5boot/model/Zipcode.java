8cc9288247be76b1eec7d36b1c0573ba032acd0e
Moonsun23 <socoooool90@gmail.com>
2023-07-07 16:22:01 +0900

    우편번호 조회 : Zipcode 클래스 추가

---
+++ src/main/java/hi1237/hello/boot/spring5boot/model/Zipcode.java
@@ -0,0 +1,19 @@
+package hi1237.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Zipcode {
+
+    private String zipcode;
+    private String sido;
+    private String gugun;
+    private String dong;
+    private String ri;
+    private String bunji;
+}
