802ed3d94923bdefe0c467dfe982af36d0e98311
Moonsun23 <socoooool90@gmail.com>
2023-07-07 11:56:22 +0900

    회원가입 : MemberDAO 단위테스트(성공!)

--- src/test/java/hi1237/hello/boot/spring5boot/MemberDAOUnitTest.java
+++ src/test/java/hi1237/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -1,21 +1,21 @@
 package hi1237.hello.boot.spring5boot;
 
 
-import hi1237.hello.boot.spring5boot.dao.Member;
+import hi1237.hello.boot.spring5boot.model.Member;
 import hi1237.hello.boot.spring5boot.dao.MemberDAO;
 import hi1237.hello.boot.spring5boot.dao.MemberDAOImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
+
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
         // 여기 내용이랑 아래 오류가 연관: 내가 쓰는 DB 시스템을 대입시켜라!
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MemberDAOUnitTest {
 
     @Autowired
-    private MemberDAO mdao;
+    private MemberDAO memberDAOImpl;
 
     @Test
     @DisplayName("MemberDAO Test")   // 아래 void를 안쓰면 MemberDAO insert Test라고 써야...
@@ -33,7 +33,7 @@ public class MemberDAOUnitTest {
 
         Member m = new Member(null, "","","","","","","","", null);
 
-        int result = mdao.insertMember(m);
+        int result = memberDAOImpl.insertMember(m);
         System.out.println(result);
         assertEquals(result, 1);
 
