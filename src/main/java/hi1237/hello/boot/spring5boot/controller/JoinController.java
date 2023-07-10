4ffa4da582c2b62d87714650042b982269c0d1ad
Moonsun23 <socoooool90@gmail.com>
2023-07-10 15:48:45 +0900

    아이디 중복검사: JoinController 구현

--- src/main/java/hi1237/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/JoinController.java
@@ -7,10 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -109,7 +106,23 @@ public class JoinController {
 
     }
 
+    // 아이디 중복검사
+    // /join/checkuid?uid=아이디
+    // /join/checkuid/아이디
+    @GetMapping("/checkuid/{uid}")
+    @ResponseBody
 
+    public void checkuid(@PathVariable String uid, HttpServletResponse res) throws IOException {
+    // 위 GetMapping의 url을 변수처럼 사용해주려면 pathvariable 사용
+
+        res.setContentType("application/json; charset=utf-8");
+        // 검색된 결과를 뷰 없이 바로 응답 response로 출력
+        res.getWriter().print(msrv.checkuid(uid));
+
+
+
+
+    }
 
 
 
