90a7fcdd9b83633b4c6c7d2c621fcd3332a9153a
Moonsun23 <socoooool90@gmail.com>
2023-07-10 15:49:45 +0900

    아이디 중복검사: checkuid 구현

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -137,3 +137,62 @@ dong?.addEventListener('keydown', (e) =>{
     if(e.keyCode === 13)            // 엔터키(13)가 입력되면
         e.preventDefault();             // 이벤트 전파 방지
 });
+
+// 비밀번호 확인
+
+let pwd = document.joinfrm.passwd;
+let repwd = document.joinfrm.repasswd;
+let pwdmsg = document.querySelector("#pwdmsg");
+repwd?.addEventListener('blur', ()=>{
+    let pmsg  = '비밀번호가 서로 일치하지 않습니다!';
+    pwdmsg.className = 'text-danger';
+
+    if (pwd.value === repwd.value) {
+        pmsg = '비밀번호가 서로 일치합니다!';
+        pwdmsg.className = 'text-primary';
+    }
+
+    pwdmsg.innerText = pmsg;
+
+});
+
+// 아이디 중복 검사
+let userid = document.joinfrm.userid;
+let checkuid = document.joinfrm.checkuid;
+let uidmsg = document.querySelector("#uidmsg");
+
+const styleCheckuid = (chkuid) => {
+    let umsg  = '사용 불가능한 아이디입니다';
+    uidmsg.className = 'text-danger';
+    checkuid.value = 'no';
+
+    if (chkuid === '0') {
+        umsg = '사용가능한 아이디입니다!';
+        uidmsg.className = 'text-primary';
+        checkuid.value = 'yes';
+    }
+
+    uidmsg.innerText = umsg;
+};
+userid?.addEventListener('blur', ()=> {
+    if (userid.value === '') {
+        uidmsg.innerText = '6~16자의 영문 소문자, 숫자와 특수기호(_)만 사용가능'
+        uidmsg.className= 'text-warning';
+        checkuid.value = 'no';
+        return;
+    }
+    const url= '/join/checkuid/' +userid.value;
+    fetch(url).then(response => response.text())
+        .then(text => styleCheckuid(text));
+});
+
+
+
+
+
+
+
+
+
+
+
