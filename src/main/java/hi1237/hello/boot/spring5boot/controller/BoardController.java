ee4255baf713b05f394f793ee42a7cef0b6213dc
Moonsun23 <socoooool90@gmail.com>
2023-07-12 18:06:08 +0900

    게시판 : BoardController writeok 구현

--- src/main/java/hi1237/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/BoardController.java
@@ -1,5 +1,6 @@
 package hi1237.hello.boot.spring5boot.controller;
 
+import hi1237.hello.boot.spring5boot.model.Board;
 import hi1237.hello.boot.spring5boot.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -7,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,6 +42,27 @@ public class BoardController {
         //bsrv.readBoard를 실행해서 넘어온 결과가 bds로 넘어옴
 
         return "board/view";
+
+    }
+
+    @GetMapping("/write")
+    public String write(){
+        logger.info("board/write 호출!");
+
+        return "board/write";
+
+    }
+
+    @PostMapping("/write")
+    public String writeok(Board b){
+        logger.info("board/writeok 호출!");
+        String returnPage = "redirect:/board/fail";
+
+        if(bsrv.saveBoard(b))
+            returnPage = "redirect:/board/list/1";
+
+
+        return returnPage;
 
     }
 
