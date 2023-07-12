856df5ddc2ef3041634f8191b6ea37d04ecf47b9
Moonsun23 <socoooool90@gmail.com>
2023-07-12 10:13:27 +0900

    게시판 : board mapper 단위테스트

---
+++ src/test/java/hi1237/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -0,0 +1,41 @@
+package hi1237.hello.boot.spring5boot.board;
+
+
+import hi1237.hello.boot.spring5boot.model.Board;
+import hi1237.hello.boot.spring5boot.model.Member;
+import hi1237.hello.boot.spring5boot.mybatis.BoardMapper;
+import hi1237.hello.boot.spring5boot.mybatis.MemberMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+        // 여기 내용이랑 아래 오류가 연관: 내가 쓰는 DB 시스템을 대입시켜라!
+
+public class BoardMapperUnitTest {
+
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Test
+    @DisplayName("boardMapper select Test")
+    void selectBoard() {
+
+       List<Board> results = boardMapper.selectBoard();
+        System.out.println(results);
+        assertNotNull(results);
+
+
+    }
+
+
+}
