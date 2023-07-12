b93e2507e7aae0649a7be7ee2a6e500638ab20ec
Moonsun23 <socoooool90@gmail.com>
2023-07-12 10:15:08 +0900

    게시판 : boardDAO 단위테스트

---
+++ src/test/java/hi1237/hello/boot/spring5boot/board/BoardDAOUnitTest.java
@@ -0,0 +1,47 @@
+package hi1237.hello.boot.spring5boot.board;
+
+
+import hi1237.hello.boot.spring5boot.dao.BoardDAO;
+import hi1237.hello.boot.spring5boot.dao.BoardDAOImpl;
+import hi1237.hello.boot.spring5boot.dao.MemberDAO;
+import hi1237.hello.boot.spring5boot.dao.MemberDAOImpl;
+import hi1237.hello.boot.spring5boot.model.Board;
+import hi1237.hello.boot.spring5boot.model.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+        // 여기 내용이랑 아래 오류가 연관: 내가 쓰는 DB 시스템을 대입시켜라!
+@Import(BoardDAOImpl.class)
+// 아래 Autowired로 MemberDAO 내용을 데려와야하는데 못데려와서 위 import를 다시 해줌
+
+public class BoardDAOUnitTest {
+
+    @Autowired
+    private BoardDAO bdao;
+
+    @Test
+    @DisplayName("BoardDAO select Test")
+    void selectBoard() {
+
+        List<Board> results = bdao.selectBoard();
+
+       // System.out.println(results);
+        assertNotNull(results);
+
+
+    }
+
+
+}
