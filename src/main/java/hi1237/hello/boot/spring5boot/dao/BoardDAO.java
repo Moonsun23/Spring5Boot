0e691b6b02af01a407950331b7e758d70953be50
Moonsun23 <socoooool90@gmail.com>
2023-07-12 11:44:32 +0900

    게시판 : boardDAO 페이징 처리 구현

--- src/main/java/hi1237/hello/boot/spring5boot/dao/BoardDAO.java
+++ src/main/java/hi1237/hello/boot/spring5boot/dao/BoardDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface BoardDAO {
     int insertBoard(Board b);
 
-    List<Board> selectBoard();
+    List<Board> selectBoard(int stnum);
 
     Board selectOneBoard(String bno);
 
--- src/main/java/hi1237/hello/boot/spring5boot/dao/BoardDAOImpl.java
+++ src/main/java/hi1237/hello/boot/spring5boot/dao/BoardDAOImpl.java
@@ -3,7 +3,6 @@ package hi1237.hello.boot.spring5boot.dao;
 import hi1237.hello.boot.spring5boot.model.Board;
 import hi1237.hello.boot.spring5boot.mybatis.BoardMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,8 +22,9 @@ public class BoardDAOImpl implements BoardDAO{
     }
 
     @Override
-    public List<Board> selectBoard() {
-        return boardMapper.selectBoard();
+    public List<Board> selectBoard(int stnum) {
+
+        return boardMapper.selectBoard(stnum);
     }
 
     @Override
