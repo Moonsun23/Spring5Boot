4ef0c8274e4b4f1c55726fda657a7837714ab742
Moonsun23 <socoooool90@gmail.com>
2023-07-12 10:16:16 +0900

    게시판 : boardService 구현

---
+++ src/main/java/hi1237/hello/boot/spring5boot/service/BoardService.java
@@ -0,0 +1,15 @@
+package hi1237.hello.boot.spring5boot.service;
+
+import hi1237.hello.boot.spring5boot.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    boolean saveBoard(Board b);
+            // 데이터 추가하는거
+
+    List<Board> readBoard();
+
+    Board readOneBoard(String bno);
+}
---
+++ src/main/java/hi1237/hello/boot/spring5boot/service/BoardServiceImpl.java
@@ -0,0 +1,30 @@
+package hi1237.hello.boot.spring5boot.service;
+
+import hi1237.hello.boot.spring5boot.dao.BoardDAO;
+import hi1237.hello.boot.spring5boot.model.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("bsrv")
+@RequiredArgsConstructor
+
+public class BoardServiceImpl implements BoardService {
+
+    final BoardDAO bdao;
+    @Override
+    public boolean saveBoard(Board b) {
+        return false;
+    }
+
+    @Override
+    public List<Board> readBoard() {
+        return bdao.selectBoard();
+    }
+
+    @Override
+    public Board readOneBoard(String bno) {
+        return null;
+    }
+}
