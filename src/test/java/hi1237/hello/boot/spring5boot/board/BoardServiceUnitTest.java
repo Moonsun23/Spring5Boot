7eb53594b25e2bf20981d6157dc68533f0cef2fc
Moonsun23 <socoooool90@gmail.com>
2023-07-12 10:16:39 +0900

    게시판 : boardService 단위테스트

---
+++ src/test/java/hi1237/hello/boot/spring5boot/board/BoardServiceUnitTest.java
@@ -0,0 +1,50 @@
+package hi1237.hello.boot.spring5boot.board;
+
+
+import hi1237.hello.boot.spring5boot.dao.BoardDAOImpl;
+import hi1237.hello.boot.spring5boot.dao.MemberDAOImpl;
+import hi1237.hello.boot.spring5boot.model.Board;
+import hi1237.hello.boot.spring5boot.model.Member;
+import hi1237.hello.boot.spring5boot.service.BoardService;
+import hi1237.hello.boot.spring5boot.service.BoardServiceImpl;
+import hi1237.hello.boot.spring5boot.service.MemberService;
+import hi1237.hello.boot.spring5boot.service.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+        // 여기 내용이랑 아래 오류가 연관: 내가 쓰는 DB 시스템을 대입시켜라!
+@Import({BoardServiceImpl.class, BoardDAOImpl.class})
+// 아래 Autowired로 MemberDAO 내용을 데려와야하는데 못데려와서 위 import를 다시 해줌
+
+public class BoardServiceUnitTest {
+
+    @Autowired
+    private BoardService bsrv;
+
+
+
+    @Test
+    @DisplayName("BoardService read Test")
+    void readBoard() {
+
+        List<Board> results = bsrv.readBoard();
+        //System.out.println(results);
+        assertNotNull(results);
+
+
+    }
+
+
+}
