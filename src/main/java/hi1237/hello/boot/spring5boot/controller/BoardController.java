175f44de4718f6a43a39b7adc33d1be33c98db95
Moonsun23 <socoooool90@gmail.com>
2023-07-13 14:20:24 +0900

    게시판 리스트 예외처리 - 존재하지않는 입력페이지 처리 (내 게시글은 70페이지까지 있는데 주소창에 100페이지를 찍었을 때 다시 1페이지로 가도록)

--- src/main/java/hi1237/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/BoardController.java
@@ -29,8 +29,16 @@ public class BoardController {
         m.addAttribute("bds", bsrv.readBoard(cpg));
         //bsrv.readBoard를 실행해서 넘어온 결과가 bds로 넘어옴
         m.addAttribute("cpg", cpg);
+
+        int cntpg = bsrv.countBoard();
         m.addAttribute("cntpg", bsrv.countBoard());
         m.addAttribute("stpg", ((cpg -1) / 10) *10 +1);
+
+        // 만일 ??가 ???보다 크다면
+        // cpg를 1로 강제 초기화(1페이지로 강제이동)
+        if ( cpg > cntpg ) {
+            return "redirect:/board/list/1";
+        }
 
 
         return "board/list";
