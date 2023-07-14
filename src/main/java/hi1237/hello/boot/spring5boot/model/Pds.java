508d829602d8ab2cdf75ed15176c9d30488b6288
Moonsun23 <socoooool90@gmail.com>
2023-07-14 16:04:48 +0900

    자료실 : Pds 클래스 추가

---
+++ src/main/java/hi1237/hello/boot/spring5boot/model/Pds.java
@@ -0,0 +1,22 @@
+package hi1237.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Pds {
+
+    private String pno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+}
+
