b464ce4eacb6be5a373760e410872297888ebda4
Moonsun23 <socoooool90@gmail.com>
2023-07-14 16:02:44 +0900

    자료실 - pds, pdsattach 테이블 추가

--- spring5boot.sql
+++ spring5boot.sql
@@ -65,3 +65,39 @@ VALUES ('abc789', '우비를 입자', '더워죽겠어요','115.92.164.155');
 select count(userid) cnt, ceil(count(userid) / 25) from board2;
 
 
+select * from board2
+where title like '%비가%';
+
+-- pds
+
+create table pds (
+                     pno                     int                     auto_increment,
+                     title                    varchar(100)       not null,
+                     userid                  varchar(18)          not null,
+                     regdate                datetime             default current_timestamp,
+                     thumbs                 int                     default 0,
+                     views                   int                     default 0,
+                     contents               text                   not null,
+                     ipaddr                  varchar(15)           not null,
+                     primary key         (pno)
+
+);
+
+alter table pds
+    add constraint fkpuid                -- 위에 foreign key 대신 여기 alter table로 써줘서 나중에 fkuid라는 값으로만 가져올 수 있게 해줌
+        foreign key (userid) references member2 (userid);
+
+create table pdsattach (
+    pano                        int                         auto_increment,
+    pno                         int                             not null,   -- 게시글 번호
+    fname                       varchar(200)              not null,  --  파일 이름, 날짜도 포함됨, uuid 포함(식별코드/ 이름이 중복될 경우를 대비한 절대중복되지 않는 유일무이한 코드)
+    ftype                        varchar(3)                 not null,  -- 파일 종류(jpg등)
+    fsize                         float                         not null,
+    fdown                       int                            default 0,  -- 다운로드
+    primary key (pano)
+
+);
+
+alter table pdsattach
+    add constraint fkpno                -- 위에 foreign key 대신 여기 alter table로 써줘서 나중에 fkuid라는 값으로만 가져올 수 있게 해줌
+        foreign key (pno) references pds (pno);
