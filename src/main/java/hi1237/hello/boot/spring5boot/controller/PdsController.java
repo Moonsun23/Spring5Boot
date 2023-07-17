ca6d6e48a1f285eaf7f37498db453dbe5c6291ac
Moonsun23 <socoooool90@gmail.com>
2023-07-17 13:41:23 +0900

    자료실 - PdsController: list, view 구현/ write 단위테스트

--- src/main/java/hi1237/hello/boot/spring5boot/controller/PdsController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/PdsController.java
@@ -1,61 +1,93 @@
 package hi1237.hello.boot.spring5boot.controller;
 
 import hi1237.hello.boot.spring5boot.model.Pds;
-
 import hi1237.hello.boot.spring5boot.service.PdsService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
-
-//지시문을 써야한다.(아래 컨트롤러)
 @Controller
 @RequestMapping("/pds")
 @RequiredArgsConstructor
-
 public class PdsController {
-
-    final PdsService psrv;
 
     Logger logger = LogManager.getLogger(PdsController.class);
 
+    final PdsService psrv;
+
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg){
+        logger.info("pds/list 호출!");
+
+        m.addAttribute("pds", psrv.readPds(cpg));
+        m.addAttribute("cpg", cpg);
+        m.addAttribute("cntpg", psrv.countPds());
+        m.addAttribute("stpg", ((cpg -1) / 10) *10 +1);
+
+        // 만일 현재페이지가 총페이지수보다 크다면
+        // cpg를 1로 강제 초기화(1페이지로 강제이동)
+        if ( cpg > (int)m.getAttribute("cntpg")) {
+            return "redirect:/pds/list/1";
+        }
+
+
+        return "pds/list";
+
+    }
+
     @GetMapping("/list")
-    public String list(){
-        logger.info("gallery/list 호출!");
+    public String list() {
+        logger.info("pds/list 호출!!");
+
         return "list-";
     }
 
     @GetMapping("/write")
-    public String write(){
-        logger.info("pds/write 호출!");
+    public String write() {
+        logger.info("pds/write 호출!!");
 
         return "pds/write";
-
     }
 
     @PostMapping("/write")
-    public String writeok(Pds p, MultipartFile attach){
-        logger.info("pds/writeok 호출!");
+    public String writeok(Pds p, MultipartFile attach) {
+        logger.info("pds/writeok 호출!!");
+
         String returnPage = "redirect:/pds/fail";
 
-
-        // 작성한 게시글을 먼저 DB에 저장하고 글번호를 알아냄(알아내서 pinfo로 값 넘기기)
+        // 작성한 게시글을 먼저 디비에 저장하고 글번호를 알아냄
         int pno = psrv.newPds(p);
 
-        // 알아낸 글 번호를 이용해서 첨부파일 처리(DB저장 과 업로드)
-        if (!attach.isEmpty()) {     //첨부파일이 존재한다면
+        // 알아낸 글번호를 이용해서 첨부파일 처리 (디비저장, 업로드)
+        if (!attach.isEmpty()) {// 첨부파일이 존재한다면
             psrv.newPdsAttach(attach, pno);
             returnPage = "redirect:/pds/list/1";
         }
+
         return returnPage;
+    }
+
+    @GetMapping("/view/{pno}")
+    public String view(Model m, @PathVariable String pno){
+        logger.info("pds/view 호출!");
+
+        m.addAttribute("p", psrv.readOnePds(pno));
+        //bsrv.readBoard를 실행해서 넘어온 결과가 bds로 넘어옴
+
+        return "pds/view";
 
     }
+
+
 
 }
