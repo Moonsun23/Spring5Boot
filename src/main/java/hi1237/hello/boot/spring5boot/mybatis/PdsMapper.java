58f952ce2e8c24718ab0f877178409b3608f427f
Moonsun23 <socoooool90@gmail.com>
2023-07-17 13:38:28 +0900

    자료실 : list, view : selectPds, selectOnePds 구현, resultMap으로 Pds, PdsAttach Join 결과 합치기

--- src/main/java/hi1237/hello/boot/spring5boot/mybatis/PdsMapper.java
+++ src/main/java/hi1237/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -1,38 +1,34 @@
 package hi1237.hello.boot.spring5boot.mybatis;
 
-import hi1237.hello.boot.spring5boot.model.Pds;
-
-import hi1237.hello.boot.spring5boot.model.PdsAttach;
 import org.apache.ibatis.annotations.Mapper;
+import hi1237.hello.boot.spring5boot.model.Pds;
+import hi1237.hello.boot.spring5boot.model.PdsAttach;
 
-
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface PdsMapper {
-
     int insertPds(Pds b);
-    // 자료실 저장
 
     int lastPdsPno();
 
     int insertPdsAttach(PdsAttach pa);
 
-}
+    List<Pds> selectPds(int stnum);
 
-    // 자료실의 첨부파일 저장
+    Pds selectOnePds(String pno);
 
-//    List<Pds> selectpds(int stnum);
-//
-//    Pds selectOnePds(String bno);
-//
+    int selectCountPds();
+
 //    int updateViewPds(String bno);
-//
-//    int selectCountPds();
 //
 //    List<Pds> selectFindPds(Map<String, Object> params);
 //
-//    int countFindpds(Map<String, Object> params);
-//    pdsMapper에서 추가하고 여기로 넘어옴
+//    int countFindPds(Map<String, Object> params);
+}
+
+
 
 
 
