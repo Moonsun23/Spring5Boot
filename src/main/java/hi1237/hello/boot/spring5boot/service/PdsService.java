24d9a2192930b86cb9de13870769b3f70ad67036
Moonsun23 <socoooool90@gmail.com>
2023-07-17 13:40:16 +0900

    자료실 - PdsService : readPds, readOnePds 구현

--- src/main/java/hi1237/hello/boot/spring5boot/service/PdsService.java
+++ src/main/java/hi1237/hello/boot/spring5boot/service/PdsService.java
@@ -1,16 +1,27 @@
 package hi1237.hello.boot.spring5boot.service;
 
-import hi1237.hello.boot.spring5boot.model.Pds;
 import org.springframework.web.multipart.MultipartFile;
+import hi1237.hello.boot.spring5boot.model.Pds;
 
+import java.util.List;
 import java.util.Map;
 
 public interface PdsService {
 
     int newPds(Pds p);
-    // return타입 ....
 
     boolean newPdsAttach(MultipartFile attach, int pno);
 
+    List<Pds> readPds(Integer cpg);
 
+    int countPds();
+
+    Pds readOnePds(String pno);
 }
+
+
+
+
+
+
+
--- src/main/java/hi1237/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/hi1237/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -1,36 +1,57 @@
 package hi1237.hello.boot.spring5boot.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import hi1237.hello.boot.spring5boot.dao.PdsDAO;
 import hi1237.hello.boot.spring5boot.model.Pds;
 import hi1237.hello.boot.spring5boot.model.PdsAttach;
 import hi1237.hello.boot.spring5boot.utils.PdsUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @Service("psrv")
 @RequiredArgsConstructor
-public class PdsServiceImpl implements PdsService{
+public class PdsServiceImpl implements PdsService {
+
     final PdsDAO pdao;
     final PdsUtils pdsUtils;
+
     @Override
     public int newPds(Pds p) {
+
         return pdao.insertPds(p);
     }
 
     @Override
     public boolean newPdsAttach(MultipartFile attach, int pno) {
 
-        // 첨부한 파일을 지정한 위치에 저장 후 파일정보 리턴(pa)
+        // 첨부한 파일을 지정한 위치에 저장후 파일정보 리턴
         PdsAttach pa = pdsUtils.processUpload(attach);
-        pa.setPno(pno + ""); // 문자를 숫자로 쓰려면????
-
+        pa.setPno(pno + "");
 
         // 첨부파일 정보를 디비에 저장
         int pacnt = pdao.insertPdsAttach(pa);
 
         return (pacnt > 0) ? true : false;
     }
+
+    @Override
+    public List<Pds> readPds(Integer cpg) {
+        int stnum = (cpg -1) * 25;
+        // 0부터 24번까지 가져오는 개념으로 이해.
+        return pdao.selectPds(stnum);
+    }
+
+    @Override
+    public int countPds() {
+        return pdao.selectCountPds();
+    }
+
+    @Override
+    public Pds readOnePds(String pno) {
+        return pdao.selectOnePds(pno);
+    }
+
 }
