64adc739e69fa7a2012e5d32abca43e7381f8bb8
Moonsun23 <socoooool90@gmail.com>
2023-07-19 14:03:46 +0900

    댓글 - cmtwrite, replywrite 구현,  view 에서 댓글/ 대댓글 출력

--- src/main/java/hi1237/hello/boot/spring5boot/controller/PdsController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/PdsController.java
@@ -1,6 +1,7 @@
 package hi1237.hello.boot.spring5boot.controller;
 
 import hi1237.hello.boot.spring5boot.model.Pds;
+import hi1237.hello.boot.spring5boot.model.PdsComment;
 import hi1237.hello.boot.spring5boot.service.PdsService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -90,6 +91,9 @@ public class PdsController {
         m.addAttribute("p", psrv.readOnePds(pno));
         //bsrv.readBoard를 실행해서 넘어온 결과가 bds로 넘어옴
 
+        //댓글부분 추가(아래)
+        m.addAttribute("pcs", psrv.readPdsComment(pno)); // 댓글, 대댓글
+
         return "pds/view";
 
     }
@@ -110,6 +114,40 @@ public class PdsController {
                 .headers((HttpHeaders)objs.get("header"))
                 .body((UrlResource)objs.get("resource"));
 
+    }
+
+    @PostMapping("/cmt/write")
+    public String cmtwriteok(PdsComment pc) {
+        logger.info("pds/cmt/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+
+        // 작성한 댓글을 테이블에 저장
+        if (psrv.newPdsComment(pc)){
+
+            // 작성한 댓글을 확인하기 위해 바로 본문출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
+    }
+
+    @PostMapping("/reply/write")
+    public String rpywriteok(PdsComment pc) {
+        logger.info("pds/reply/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+
+        // 작성한 대댓글을 테이블에 저장
+        if (psrv.newPdsReply(pc)){
+
+            // 작성한 대댓글을 확인하기 위해 바로 본문출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
     }
 
 
