37be0c61ab5c3f3744215bc74a3207799192fa0d
Moonsun23 <socoooool90@gmail.com>
2023-07-20 11:19:14 +0900

    갤러리 : GalleryDAO 추가

---
+++ src/main/java/hi1237/hello/boot/spring5boot/dao/GalleryDAOImpl.java
@@ -0,0 +1,19 @@
+package hi1237.hello.boot.spring5boot.dao;
+
+import hi1237.hello.boot.spring5boot.model.Gallery;
+import hi1237.hello.boot.spring5boot.mybatis.GalleryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("gdao")
+public class GalleryDAOImpl implements GalleryDAO{
+
+    @Autowired
+    private GalleryMapper galleryMapper;
+    @Override
+    public List<Gallery> selectGallery(int stnum) {
+        return galleryMapper.selectGallery(stnum);
+    }
+}
