b87160bff92d0da6bb1709d775637a39e5a8fd43
Moonsun23 <socoooool90@gmail.com>
2023-07-20 11:19:35 +0900

    갤러리 : GalleryService 추가

---
+++ src/main/java/hi1237/hello/boot/spring5boot/service/GalleryService.java
@@ -0,0 +1,9 @@
+package hi1237.hello.boot.spring5boot.service;
+
+import java.util.List;
+
+public interface GalleryService {
+    List<Gallery> readGallery(Integer cpg);
+
+    int countGallery();
+}
---
+++ src/main/java/hi1237/hello/boot/spring5boot/service/GalleryServiceImpl.java
@@ -0,0 +1,24 @@
+package hi1237.hello.boot.spring5boot.service;
+
+import hi1237.hello.boot.spring5boot.dao.GalleryDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("gsrv")
+@RequiredArgsConstructor
+
+public class GalleryServiceImpl implements GalleryService{
+
+    final GalleryDAO gdao;
+    @Override
+    public List<Gallery> readGallery(Integer cpg) {
+        return gdao.selectGallery( cpg - 1);
+    }
+
+    @Override
+    public int countGallery() {
+        return 0;
+    }
+}
