da91eea6f6b71280a2b453418de8ae92c53ce9c8
Moonsun23 <socoooool90@gmail.com>
2023-07-20 11:19:53 +0900

    갤러리 : GalleryController list 구현

--- src/main/java/hi1237/hello/boot/spring5boot/controller/GalleryController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/GalleryController.java
@@ -1,25 +1,42 @@
 package hi1237.hello.boot.spring5boot.controller;
 
+import hi1237.hello.boot.spring5boot.service.GalleryService;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/gallery")
+@RequiredArgsConstructor
+
+
 public class GalleryController {
+
+    final GalleryService gsrv;
 
     Logger logger = LogManager.getLogger(GalleryController.class);
 
-    @GetMapping("/list")
-    public String list(){
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg) {
         logger.info("gallery/list 호출!");
 
+        m.addAttribute("gal", gsrv.readGallery(cpg));
+        m.addAttribute("cpg", cpg);
+        m.addAttribute("cntpg", gsrv.countGallery());
+        m.addAttribute("stpg", ((cpg - 1) / 10) * 10 + 1);
+
+        // 만일 현재페이지가 총페이지수보다 크다면
+        // cpg를 1로 강제 초기화(1페이지로 강제이동)
+        if (cpg > (int) m.getAttribute("cntpg")) {
+            return "redirect:/gallery/list/1";
+        }
+
         return "gallery/list";
-
     }
-
-
-
 }
+
