51b27c9998940172fc983dbb346634a8b07fae49
Moonsun23 <socoooool90@gmail.com>
2023-07-21 10:52:46 +0900

    갤러리 : write, writeok 구현

--- src/main/java/hi1237/hello/boot/spring5boot/controller/GalleryController.java
+++ src/main/java/hi1237/hello/boot/spring5boot/controller/GalleryController.java
@@ -1,5 +1,7 @@
 package hi1237.hello.boot.spring5boot.controller;
 
+import hi1237.hello.boot.spring5boot.model.Gallery;
+import hi1237.hello.boot.spring5boot.model.Pds;
 import hi1237.hello.boot.spring5boot.service.GalleryService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -8,7 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/gallery")
@@ -32,11 +38,38 @@ public class GalleryController {
 
         // 만일 현재페이지가 총페이지수보다 크다면
         // cpg를 1로 강제 초기화(1페이지로 강제이동)
-        if (cpg > (int) m.getAttribute("cntpg")) {
-            return "redirect:/gallery/list/1";
-        }
+//        if (cpg > (int) m.getAttribute("cntpg")) {
+//            return "redirect:/gallery/list/1";
+//        }
 
         return "gallery/list";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        logger.info("gallery/write 호출!!");
+
+        return "gallery/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Gallery g, List<MultipartFile> attachs) {
+        logger.info("gallery/writeok 호출!!");
+
+        String returnPage = "redirect:/gallery/fail";
+
+        // 작성한 게시글을 먼저 디비에 저장하고 글번호를 알아냄
+        int gno = gsrv.newGallery(g);
+
+        // 알아낸 글번호를 이용해서 첨부파일 처리 (디비저장, 업로드)
+        if (!attachs.isEmpty()) {// 첨부파일이 존재한다면
+            gsrv.newGalAttach(attachs, gno);
+            returnPage = "redirect:/gallery/list/1";
+        }
+
+        // ctrl + alt + b 누르면 어디로 이어지는지 나옴
+
+        return returnPage;
     }
 }
 
