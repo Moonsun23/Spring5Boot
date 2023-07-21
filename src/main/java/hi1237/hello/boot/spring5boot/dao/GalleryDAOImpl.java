aca820124cd8e9d53b8c0dffadb2aa2ce60c3a33
Moonsun23 <socoooool90@gmail.com>
2023-07-21 10:48:54 +0900

    갤러리 : insertGallery, insertGalAttach 구현

--- src/main/java/hi1237/hello/boot/spring5boot/dao/GalleryDAOImpl.java
+++ src/main/java/hi1237/hello/boot/spring5boot/dao/GalleryDAOImpl.java
@@ -1,5 +1,6 @@
 package hi1237.hello.boot.spring5boot.dao;
 
+import hi1237.hello.boot.spring5boot.model.GalAttach;
 import hi1237.hello.boot.spring5boot.model.Gallery;
 import hi1237.hello.boot.spring5boot.mybatis.GalleryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,17 @@ public class GalleryDAOImpl implements GalleryDAO{
     @Override
     public List<Gallery> selectGallery(int stnum) {
         return galleryMapper.selectGallery(stnum);
+    }
+
+    @Override
+    public int insertGallery(Gallery g) {
+        int cnt = galleryMapper.insertGallery(g);
+        if (cnt > 0) cnt = galleryMapper.lastGalGno();
+        return cnt;
+    }
+
+    @Override
+    public int insertGalAttach(GalAttach ga) {
+        return galleryMapper.insertGalAttach(ga);
     }
 }
